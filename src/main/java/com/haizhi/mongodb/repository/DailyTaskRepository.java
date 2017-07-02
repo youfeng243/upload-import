50c3e66d11123d678654d70db66ec43ffe48e40e
youfeng <youfeng@haizhi.com>
2017-07-02 18:50:07 +0800

    调整数据结构名称

--- src/main/java/com/haizhi/mongodb/repository/DailyTaskRepository.java
+++ src/main/java/com/haizhi/mongodb/repository/DailyTaskRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 /**
  * Created by youfeng on 2017/7/2.
+ * 每日任务mongodb访问接口
  */
 public interface DailyTaskRepository extends MongoRepository<DailyTask, String> {
     DailyTask findByDate(String date);
