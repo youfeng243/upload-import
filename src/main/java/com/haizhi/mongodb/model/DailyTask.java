6b2f78875739f7af7b44d086e8f391596e5e4a91
youfeng <youfeng@haizhi.com>
2017-07-02 18:07:04 +0800

    代码备份

--- src/main/java/com/haizhi/mongodb/model/DailyTask.java
+++ src/main/java/com/haizhi/mongodb/model/DailyTask.java
@@ -1,5 +1,6 @@
 package com.haizhi.mongodb.model;
 
+import com.haizhi.util.TimeUtil;
 import org.springframework.data.annotation.Id;
 
 import java.util.Map;
@@ -16,6 +17,33 @@ public class DailyTask {
 
     // 不同类型数据导入
     private Map<String, FolderDetail> folderDetailMap;
+
+    // 任务创建时间
+    private String createTime;
+
+    // 任务更新时间
+    private String updateTime;
+
+    public DailyTask(String date) {
+        this.date = date;
+        this.createTime = this.updateTime = TimeUtil.getCurrentTime();
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
 
     public String getDate() {
         return date;
--- src/main/java/com/haizhi/schedule/ScheduleTask.java
+++ src/main/java/com/haizhi/schedule/ScheduleTask.java
@@ -1,11 +1,16 @@
 package com.haizhi.schedule;
 
+import com.haizhi.mongodb.model.DailyTask;
+import com.haizhi.mongodb.service.DailyTaskService;
+import com.haizhi.util.TimeUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
 
 /**
  * Created by youfeng on 2017/7/2.
@@ -24,10 +29,13 @@ public class ScheduleTask {
     //上传基本根路径
     private String uploadBasePath;
 
+    private final DailyTaskService dailyTaskService;
+
     @Autowired
-    public ScheduleTask(Environment environment) {
+    public ScheduleTask(Environment environment, DailyTaskService dailyTaskService) {
 
         this.scanPeriod = environment.getProperty("check.period", int.class);
+        this.dailyTaskService = dailyTaskService;
         logger.info("扫描周期: {}", this.scanPeriod);
         this.threadNum = environment.getProperty("exec.task.num", int.class);
         logger.info("线程池数目: {}", this.threadNum);
@@ -38,6 +46,35 @@ public class ScheduleTask {
     @Scheduled(fixedDelay = 1000 * 60 * 30)
     public void scanFolder() {
         logger.info("#####开始扫描文件..");
+
+        for (int i = 0; i < scanPeriod; i++) {
+
+            String date = TimeUtil.getBeforeDate(i);
+
+            //先判断批次任务是否已经上传上来了
+            String batchPath = uploadBasePath + date;
+
+            logger.info("当前检测的路径为: {}", batchPath);
+
+            File file = new File(batchPath);
+
+            //判断批次路径是否已经存在 如果不存在则不进行检测
+            if (!file.isDirectory() || !file.exists()) {
+                logger.info("当前目录不存在: {}", batchPath);
+                continue;
+            }
+
+            // 获得数据库中批次信息
+            DailyTask dailyTask = dailyTaskService.findOne(date);
+            if (dailyTask == null) {
+                dailyTask = new DailyTask(date);
+                logger.info("还未针对当前批次建立任务: {}", date);
+            }
+
+            //开始分析每个目录每个文件是否已经被记录
+
+        }
+
     }
 
 }
